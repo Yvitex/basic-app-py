a2fe9eb39938ff5c5eabf77696ca77aa76356d50
Yvitex <opinionjaymar1@gmail.com>
2026-08-02 18:01:59 +0800

    init

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import datetime
+import socket
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify({
+        'time': datetime.datetime.now().isoformat(),
+        'host': socket.gethostname()
+    })
+
+
+@app.route('/api/v1/health')
+def health():
+    return jsonify({'status': ' Up'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
